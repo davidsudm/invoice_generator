8c737dadfdb5a9b1ca8aae7108348916ffcf5da2
davidsudm <davidsudm@gmail.com>
2023-12-28 03:00:19 +0100

    Update main.py
    add a browsing folder button to save outputs

--- main.py
+++ main.py
@@ -22,13 +22,18 @@ def pick_date(entry, window):
     cal.grid(row=0, column=0, padx=10, pady=10)
     cal.config(background='lightblue', foreground='black', bordercolor='white')
 
-    select_button = tkinter.Button(top, text="Aceptar Fecha", command=lambda: on_date_selected(cal))
+    select_button = tkinter.Button(top, text="ACEPTAR FECHA", command=lambda: on_date_selected(cal))
     select_button.grid(row=1, column=0, pady=10)
 
 
 def browse_file(entry_var):
     file_path = filedialog.askopenfilename()
     entry_var.set(file_path)
+
+
+def browse_folder(entry_var):
+    folder_path = filedialog.askdirectory()
+    entry_var.set(folder_path)
 
 
 def main():
@@ -100,37 +105,46 @@ def main():
     # Create an Entry widget to display the selected file path
     open_csv_file_entry = tkinter.Entry(frame, textvariable=open_csv_file_entry_var, width=50)
     open_csv_file_entry.grid(row=7, column=1, columnspan=2, padx=10, pady=20)
-
     # Open the file dialog
-    open_csv_file_button = tkinter.Button(frame, text="Buscar Tablero CSV",
-                                     command=lambda: browse_file(open_csv_file_entry_var))
+    open_csv_file_button = tkinter.Button(frame, text="BUSCAR TABLERO CSV",
+                                          command=lambda: browse_file(open_csv_file_entry_var))
     open_csv_file_button.grid(row=7, column=0, sticky="news", padx=20, pady=20)
+
+    # Create a StringVar to hold the folder path
+    open_folder_entry_var = tkinter.StringVar()
+    # Create an Entry widget to display the selected folder path
+    open_folder_entry = tkinter.Entry(frame, textvariable=open_folder_entry_var, width=50)
+    open_folder_entry.grid(row=8, column=1, columnspan=2, padx=10, pady=20)
+    # Open the folder dialog
+    open_folder_button = tkinter.Button(frame, text="GUARDAR EN CARPETA",
+                                        command=lambda: browse_folder(open_folder_entry_var))
+    open_folder_button.grid(row=8, column=0, sticky="news", padx=20, pady=20)
 
     # Optional Columns
     optional_columns = tkinter.Label(frame, text="COLUMNAS ADICIONALES (OPCIONAL)")
-    optional_columns.grid(row=8, column=0, columnspan=3, pady=20)
+    optional_columns.grid(row=9, column=0, columnspan=3, pady=20)
 
     opt_col_check = tkinter.Label(frame, text="ETIQUETA DE LA COLUMNA")
-    opt_col_check.grid(row=9, column=1, columnspan=2, pady=10)
+    opt_col_check.grid(row=10, column=1, columnspan=2, pady=10)
 
     # Checkbutton example
     check_var_1 = tkinter.IntVar()
     opt_col_1 = tkinter.Checkbutton(frame, text="AGREGAR COLUMNA 1", variable=check_var_1)
-    opt_col_1.grid(row=10, column=0, pady=10)
+    opt_col_1.grid(row=11, column=0, pady=10)
     opt_col_1_label = tkinter.Entry(frame, width=50)
-    opt_col_1_label.grid(row=10, column=1, columnspan=2, pady=10)
+    opt_col_1_label.grid(row=11, column=1, columnspan=2, pady=10)
 
     check_var_2 = tkinter.IntVar()
     opt_col_2 = tkinter.Checkbutton(frame, text="AGREGAR COLUMNA 2", variable=check_var_2)
-    opt_col_2.grid(row=11, column=0, pady=10)
+    opt_col_2.grid(row=12, column=0, pady=10)
     opt_col_2_label = tkinter.Entry(frame, width=50)
-    opt_col_2_label.grid(row=11, column=1, columnspan=2, pady=10)
+    opt_col_2_label.grid(row=12, column=1, columnspan=2, pady=10)
 
     check_var_3 = tkinter.IntVar()
     opt_col_3 = tkinter.Checkbutton(frame, text="AGREGAR COLUMNA 3", variable=check_var_3)
-    opt_col_3.grid(row=12, column=0, pady=10)
+    opt_col_3.grid(row=13, column=0, pady=10)
     opt_col_3_label = tkinter.Entry(frame, width=50)
-    opt_col_3_label.grid(row=12, column=1, columnspan=2, pady=10)
+    opt_col_3_label.grid(row=13, column=1, columnspan=2, pady=10)
 
     gen_invoice_button = tkinter.Button(frame, text="GENERAR RECIBOS")
     gen_invoice_button.config(width=20, height=3, fg='green', font=('Helvetica', 16))
